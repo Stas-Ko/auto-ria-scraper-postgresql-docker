c83e793a40b445367638979957485c4e943df3ea
Stas-Ko <142270795+Stas-Ko@users.noreply.github.com>
2024-01-23 07:44:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,75 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
 from ad_info_collector import get_current_page_info
-from sel_num_phone_fun import get_number
 from db_insert_script import insert_data
 import logging
-import os
-from datetime import datetime
 
-# Создаем папку "logs", если ее нет
-if not os.path.exists("logs"):
-    os.makedirs("logs")
 
-# Определяем имя файла для логирования
-log_file = f"logs/{datetime.now().strftime('%Y-%m-%d')}.log"
+# Настройки логирования
+logging.basicConfig(filename='main_script.log', level=logging.ERROR,
+                    format='%(asctime)s [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-# Настройка логгера
-logging.basicConfig(filename=log_file, level=logging.ERROR,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+def start_main_fun():
+    url = 'https://auto.ria.com/uk/car/used/'
 
-# Включаем логгирование в консоль
-console = logging.StreamHandler()
-console.setLevel(logging.ERROR)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-console.setFormatter(formatter)
-logging.getLogger('').addHandler(console)
+    while True:
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # Проверка наличия ошибок при запросе
 
-# Пример использования логгера
-logging.info("Скрипт запущен")
+            page_content = response.content
+            soup = BeautifulSoup(page_content, 'html.parser')
 
-url = 'https://auto.ria.com/uk/car/used/'
+            # Найдем все элементы <section> с классом "ticket-item"
+            ticket_sections = soup.find_all('section', class_='ticket-item')
 
-while True:
+            # Проходим по каждому элементу <section> и извлекаем ссылку из <a class="m-link-ticket">
+            for ticket_section in ticket_sections:
+                link_element = ticket_section.find('a', class_='m-link-ticket')
+                if link_element:
+                    link_url = link_element.get('href')
+                    try:
 
-    response = requests.get(url)
-    page_content = response.content
+                        insert_data(*get_current_page_info(link_url))
 
-    soup = BeautifulSoup(page_content, 'html.parser')
+                    except Exception as e:
+                        # Логируем исключение
+                        logging.error(f"Произошла ошибка при обработке ссылки {link_url}: {e}", exc_info=True)
 
-    # Найдем все элементы <section> с классом "ticket-item"
-    ticket_sections = soup.find_all('section', class_='ticket-item')
+            prefetch_link = soup.find('link', rel='prefetch')
+            if prefetch_link:
+                url = prefetch_link.get('href')
 
-    # Проходим по каждому элементу <section> и извлекаем ссылку из <a class="m-link-ticket">
-    for ticket_section in ticket_sections:
-        link_element = ticket_section.find('a', class_='m-link-ticket')
-        if link_element:
-            link_url = link_element.get('href')
-            print(link_url)
-            print()
-            try:
-                #url, title, price_usd, odometer, username, phone_number, image_url, images_count, car_number, car_vin = get_current_page_info(link_url)
-                #get_current_page_info(link_url)
+        except requests.RequestException as req_exc:
+            # Логируем исключение при запросе
+            logging.error(f"Произошла ошибка при запросе {url}: {req_exc}", exc_info=True)
+        except Exception as exc:
+            # Логируем общие ошибки
+            logging.error(f"Произошла неизвестная ошибка: {exc}", exc_info=True)
 
-                #print(all_data)
-                #print(get_number(link_url))
-                print('HELLO')
-
-                #insert_data(url, title, price_usd, odometer, username, phone_number, image_url, images_count,
-                            #car_number, car_vin)
-
-                insert_data(*get_current_page_info(link_url))
-
-            except Exception as e:
-                # Логируем исключение
-                logging.error(f"Произошла ошибка: {e}", exc_info=True)
-
-                #print(f"Произошла ошибка: {e}")
-  
-    prefetch_link = soup.find('link', rel='prefetch')
-
-    if prefetch_link:
-        url = prefetch_link.get('href')
-        print(url)
-    else:
-        print("Ссылка с rel='prefetch' не найдена.")
+if __name__ == "__main__":
+    start_main_fun()
